ade1e4c3de01e1a86316e7cf5e7eb303566586fe
dario007 <dariojjk@gmail.com>
2019-10-01 19:33:11 +0200

    #upload all local files

--- app/controllers/frontend/IndexController.php
+++ app/controllers/frontend/IndexController.php
@@ -2,16 +2,14 @@
 
 namespace App\Controllers\frontend;
 
-
-
 class IndexController extends BaseController
 {
 
     public function indexAction($request, $response)
     {
-
         return $this->view->render($response, '/frontend/index/index.twig');
     }
+
 }
 
 ?>
